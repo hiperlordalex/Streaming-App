844d63774df436851526e9102d58d3909f7c15e9
Alex <alex.valentee@gmail.com>
2017-06-27 00:41:56 +0200

    initialize metric analyser

---
+++ log-analyzer/src/main/java/com/alex/streamingapp/configuration/entitys/LogEvent.java
@@ -0,0 +1,7 @@
+package com.alex.streamingapp.configuration.entitys;
+
+/**
+ * Created by Valente on 2017.06.27..
+ */
+public class LogEvent {
+}
---
+++ log-analyzer/src/main/java/com/alex/streamingapp/configuration/entitys/Source.java
@@ -0,0 +1,9 @@
+package com.alex.streamingapp.configuration.entitys;
+
+/**
+ * Created by Valente on 2017.06.27..
+ */
+public enum Source {
+
+    JMX
+}
